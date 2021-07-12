ab58beba9976047a84d3134dc9eeb8340796ac64
Marc Roger Sevilla <78522331+MrSevilla1022@users.noreply.github.com>
2021-07-12 23:39:46 +0800

    landing

--- src/app/public/landingpage/landingpage.component.ts
+++ src/app/public/landingpage/landingpage.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class LandingpageComponent implements OnInit {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  
   constructor() { }
 
   ngOnInit(): void {
