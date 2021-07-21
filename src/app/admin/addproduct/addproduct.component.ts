deef841ffb376748ab45451e4d4615deaa281468
Marc Roger Sevilla <78522331+MrSevilla1022@users.noreply.github.com>
2021-07-21 21:58:14 +0800

    add product modal

--- src/app/admin/addproduct/addproduct.component.ts
+++ src/app/admin/addproduct/addproduct.component.ts
@@ -53,6 +53,14 @@ export class AddproductComponent implements OnInit {
     this.modalService.open(content);
   }
 
+  openAddBrand(branding:any) {
+    this.modalService.open(branding);
+  }
+
+  openPtype(ptype:any){
+    this.modalService.open(ptype);
+  }
+
   getBrands(){
     this.ds.sendApiRequest("brand/", null ).subscribe((data: any) => {
       console.log(data.payload);
@@ -114,47 +122,6 @@ export class AddproductComponent implements OnInit {
     reader.readAsDataURL(this.imageToUpload);
   }
 
-  countries = COUNTRIES
+
 
 }
-
-interface Country {
-  name: string;
-  flag: string;
-  area: number;
-  population: number;
-}
-
-interface Country {
-  name: string;
-  flag: string;
-  area: number;
-  population: number;
-}
-
-const COUNTRIES: Country[] = [
-  {
-    name: 'Russia',
-    flag: 'f/f3/Flag_of_Russia.svg',
-    area: 17075200,
-    population: 146989754
-  },
-  {
-    name: 'Canada',
-    flag: 'c/cf/Flag_of_Canada.svg',
-    area: 9976140,
-    population: 36624199
-  },
-  {
-    name: 'United States',
-    flag: 'a/a4/Flag_of_the_United_States.svg',
-    area: 9629091,
-    population: 324459463
-  },
-  {
-    name: 'China',
-    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    area: 9596960,
-    population: 1409517397
-  }
-];
