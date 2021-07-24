532bd7a10f47e589b39bff0bf9972c2e3470442e
Marc Roger Sevilla <78522331+MrSevilla1022@users.noreply.github.com>
2021-07-25 00:16:04 +0800

    mostly landing page

--- src/app/navigation-bar/footer/footer.component.ts
+++ src/app/navigation-bar/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+
+  windowScrolled: any;
+    constructor() {}
+    @HostListener("window:scroll", [])
+    onWindowScroll() {
+        if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+            this.windowScrolled = true;
+        } 
+       else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+            this.windowScrolled = false;
+        }
+    }
+    scrollToTop() {
+        (function smoothscroll() {
+            var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+            if (currentScroll > 0) {
+                window.requestAnimationFrame(smoothscroll);
+                window.scrollTo(0, currentScroll - (currentScroll / 8));
+            }
+        })();
+    }
 
   ngOnInit(): void {
   }
