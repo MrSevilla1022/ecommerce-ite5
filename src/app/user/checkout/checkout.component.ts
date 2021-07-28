bdbef8425a8edf66af3258154949d411bba6f2bb
dungogclarissealyanna <201810284@gordoncollege.edu.ph>
2021-07-29 03:15:41 +0800

    modal bug + UI updates

--- src/app/user/checkout/checkout.component.ts
+++ src/app/user/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-checkout',
@@ -8,30 +9,21 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class CheckoutComponent implements OnInit {
 
-  closeResult = '';
-  content: any;
+  closeResult: string = '';
+  @ViewChild('content') content: any ;
+
+  openModal(){
+    this.modalService.open(this.content, { centered: true });
+  }
 
   constructor(private modalService: NgbModal) { }
 
-  open(content: any) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
   ngOnInit(): void {
     
+  }
+
+  open(content:any) {
+    this.modalService.open(content);
   }
 
 }
